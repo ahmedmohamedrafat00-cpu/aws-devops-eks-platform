0e66888ca6b5e9ddad4431bf85f9ea72ab82def5
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-20 16:01:00 +0200

    Terraform servers-setup: initial provider and variables

---
+++ terraform/servers-setup/terraform.tfvars
@@ -0,0 +1,6 @@
+aws_region   = "eu-central-1"
+project_name = "aws-devops-eks-platform"
+environment  = "dev"
+instance_type = "t3.micro"
+key_name     = "deployer-one"
+
---
+++ terraform/servers-setup/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "aws-devops-eks"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "platform"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "deployer-one"
+}
