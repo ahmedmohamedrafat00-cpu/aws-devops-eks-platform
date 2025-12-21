858ad6be48600b4335614201e3b4bc90049a4beb
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 14:55:45 +0200

    EKS: add cluster resource skeleton

---
+++ EKS/terraform/eks.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.kubernetes_version
+
+  vpc_config {
+    subnet_ids = [
+      "subnet-xxxx",
+      "subnet-yyyy"
+    ]
+  }
+}
