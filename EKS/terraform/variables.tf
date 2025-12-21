11b0ee852848ee2370eb6437309907477783af18
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 14:51:56 +0200

    EKS: add input variables

---
+++ EKS/terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "devops-eks-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "node_instance_type" {
+  description = "EKS worker node instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_capacity" {
+  type    = number
+  default = 2
+}
