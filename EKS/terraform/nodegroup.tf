ade36fef5a8c4db3e00f9b1388b5834a8c85ba6f
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 14:58:32 +0200

    EKS: add managed node group skeleton

---
+++ EKS/terraform/nodegroup.tf
@@ -0,0 +1,18 @@
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${local.name_prefix}-nodes"
+  node_role_arn  = aws_iam_role.eks_node_role.arn
+  subnet_ids     = [
+    "subnet-xxxx",
+    "subnet-yyyy"
+  ]
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = [var.node_instance_type]
+}
+
