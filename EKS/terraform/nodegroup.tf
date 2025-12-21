dc8350b8707483db6d38b77f6e1c9eb0f317c5f2
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 19:16:24 +0200

    EKS: bind node group to private subnets

--- EKS/terraform/nodegroup.tf
+++ EKS/terraform/nodegroup.tf
@@ -2,10 +2,8 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${local.name_prefix}-nodes"
   node_role_arn  = aws_iam_role.eks_node_role.arn
-  subnet_ids     = [
-    "subnet-xxxx",
-    "subnet-yyyy"
-  ]
+  subnet_ids = var.private_subnet_ids
+
 
   scaling_config {
     desired_size = var.desired_capacity
