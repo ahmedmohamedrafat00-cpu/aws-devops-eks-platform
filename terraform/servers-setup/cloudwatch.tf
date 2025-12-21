f1ada595964ac79ce5a042b06d14d2a349f96894
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 19:47:15 +0200

    Add CloudWatch alarm design for Jenkins CPU

--- terraform/servers-setup/cloudwatch.tf
+++ terraform/servers-setup/cloudwatch.tf
@@ -7,3 +7,19 @@ resource "aws_sns_topic" "servers_alerts" {
 resource "aws_sns_topic" "eks_alerts" {
   name = "${local.name_prefix}-eks-alerts"
 }
+resource "aws_cloudwatch_metric_alarm" "jenkins_high_cpu" {
+  alarm_name          = "${local.name_prefix}-jenkins-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    InstanceId = "jenkins-instance-id"
+  }
+
+  alarm_actions = [aws_sns_topic.servers_alerts.arn]
+}
