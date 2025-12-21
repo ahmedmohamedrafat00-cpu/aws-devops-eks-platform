c7ddc075b54001e33ff8a3a0568be67dde8e6185
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-21 14:54:36 +0200

    EKS: add IAM roles skeleton

---
+++ EKS/terraform/iam.tf
@@ -0,0 +1,31 @@
+# EKS Cluster Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${local.name_prefix}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Worker Node Role
+resource "aws_iam_role" "eks_node_role" {
+  name = "${local.name_prefix}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
