e29992edab493f93e96a20f8e14d1c08b5d56520
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-23 15:39:55 +0200

    AMI AL2_x86_64

--- EKS/terraform/nodegroup.tf
+++ EKS/terraform/nodegroup.tf
@@ -1,16 +1,18 @@
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${local.name_prefix}-nodes"
+  node_group_name = "${var.cluster_name}-nodes"
   node_role_arn  = aws_iam_role.eks_node_role.arn
+
   subnet_ids = var.private_subnet_ids
 
+  instance_types = ["t3.medium"]
+
+  ami_type = "AL2_x86_64"
 
   scaling_config {
-    desired_size = var.desired_capacity
+    desired_size = 2
     max_size     = 3
     min_size     = 1
   }
-
-  instance_types = [var.node_instance_type]
 }
 
